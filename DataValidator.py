6404fe5160a347f7359de6937580ebed1de77090
Abdulhannan Shehzad <hannybutt01@gmail.com>
2023-12-13 13:57:55 +0100

    First Commit, Original Code Created to DataValidator.py

---
+++ DataValidator.py
@@ -0,0 +1,22 @@
+class ValidateData:
+    def __init__(self):
+        self.validate_data = []
+
+    def validate_data_input(self, validate_input):
+        for input in validate_input:
+            if self.is_positive_integer(input):
+                self.validate_data.append(int(input))
+
+    def valid_positive_integer(self, input):
+        try:
+            num = int(input)
+            if num > 0:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+        
+validating_data = ValidateData()
+
+user_inputs = ['1','2','3','knock knock','-1','4','5']
